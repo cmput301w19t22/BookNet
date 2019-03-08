e4e5a238e64915ac49b015aee623092532bb739e
JamiePowers4446 <46546056+JamiePowers4446@users.noreply.github.com>
2019-03-08 00:46:19 -0700

    Bone Dry UI

--- code/app/src/main/java/com/example/booknet/MainActivity.java
+++ code/app/src/main/java/com/example/booknet/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.booknet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private SearchView searchBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_search:
+                    //mTextMessage.setText(R.string.title_home);
+                    searchClicked();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_mybooks:
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    myBooksClicked();
+                    return true;
+                case R.id.navigation_myaccount:
+                    myAccountClicked();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -39,6 +47,24 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        searchBar = findViewById(R.id.mainSearch);
+
     }
 
+
+    private void myBooksClicked() {
+        Intent intent = new Intent(this, OwnedLibraryActivity.class);
+        startActivity(intent);
+    }
+
+    private void myAccountClicked() {
+        Intent intent = new Intent(this, UserProfileViewActivity.class);
+        startActivity(intent);
+    }
+
+    private void searchClicked() {
+        Intent intent = new Intent(this, BookSearchActivity.class);
+        startActivity(intent);
+    }
 }
--- code/app/src/main/java/com/example/booknet/OwnedLibraryActivity.java
+++ code/app/src/main/java/com/example/booknet/OwnedLibraryActivity.java
@@ -1,13 +1,32 @@
 package com.example.booknet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class OwnedLibraryActivity extends AppCompatActivity {
+
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owned_library);
+
+        addButton = findViewById(R.id.addBookButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBook();
+            }
+        });
+    }
+
+
+    private void addBook() {
+        Intent intent = new Intent(this, NewBookActivity.class);
+        startActivity(intent);
     }
 }
